82ed09d6917d8164c2091dbcf71c133b2b94bbcc
TisMeCastle <tismecastle@gmail.com>
2022-12-02 21:16:48 -0700

    Join DM

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const welcome = '1046555655844143146'
 const rules = '915101678540308480'
 const MCount = "915101667777712188"
-const { MessageEmbed } = require("discord.js")
+const { MessageEmbed, MessageActionRow, MessageButton, Modal, TextInputComponent, MessageSelectMenu } = require('discord.js');
 
 module.exports = {
 	name: "guildMemberAdd",
@@ -23,5 +23,31 @@ module.exports = {
 		//Member Cound Updates
 		const MemberCountChannel = member.guild.channels.cache.get(MCount);
 		MemberCountChannel.setName(`🚀┃Members: ${member.guild.memberCount}`);
+
+
+		const buttonData = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setCustomId('social')
+					.setLabel('Social Pings')
+					.setStyle('PRIMARY'),
+				new MessageButton()
+					.setCustomId('announce')
+					.setLabel('Announcement Pings')
+					.setStyle('PRIMARY'),
+				new MessageButton()
+					.setCustomId('tourney')
+					.setLabel('Tournament Pings')
+					.setStyle('PRIMARY'),
+				new MessageButton()
+					.setCustomId('twitch')
+					.setLabel('Twitch Pings')
+					.setStyle('PRIMARY'),
+				new MessageButton()
+					.setCustomId('poll')
+					.setLabel('Poll Pings')
+					.setStyle('PRIMARY')
+			)
+		member.send({ content: "**Select Your Roles!!! Once You've <#915101667018543144>, Visit <#915101682017386566> For A Wider Selection!!!**\n*Click The Button Again To Remove Roles*", components: [buttonData]});
 	}
 }
--- events/handlers/buttonhandler.js
+++ events/handlers/buttonhandler.js
@@ -1,6 +1,13 @@
 const { MessageEmbed, MessageActionRow, MessageButton, Modal, TextInputComponent } = require('discord.js');
-module.exports.buttonHandler = async (interaction) => {
+module.exports.buttonHandler = async (interaction, client) => {
+
+    //console.log(interaction.client)
+
     const option = interaction.customId;
+    const guildId = '840834053082054676';
+    const guild = await interaction.client.guilds.fetch(guildId);
+    const member = await guild.members.fetch(interaction.user.id);
+
     if (option === 'closebutt') {
         const command = interaction.client.commands.get("close");
         try {
@@ -12,6 +19,66 @@ module.exports.buttonHandler = async (interaction) => {
                 content: "An error occurred while executing that command.",
                 ephemeral: true,
             });
+        }
+    }
+
+    if (option === 'verifybutt') {
+        interaction.member.roles.add("915101645396934667")
+        interaction.member.roles.remove("915101646420328478")
+        interaction.reply({
+            content: `Welcome to Clovarity!`,
+            ephemeral: true
+        });
+    }
+
+    if (option === 'social') {
+        if (!member.roles.cache.has('915101651189260348')) {
+            member.roles.add('915101651189260348')
+            interaction.reply({ content: `You Will Now Be Notified For Anything Clovarity Uploads On Social Media!!`, ephemeral: true })
+        }
+        if (member.roles.cache.has('915101651189260348')) {
+            member.roles.remove('915101651189260348')
+            interaction.reply({ content: `You've been removed from the \`Social Pings\` role!`, ephemeral: true })
+        }
+
+    } if (option === 'announce') {
+        if (!member.roles.cache.has('915101648894963752')) {
+            member.roles.add('915101648894963752')
+            interaction.reply({ content: `You've been given the \`Announcement Pings\` role!`, ephemeral: true })
+        }
+        if (member.roles.cache.has('915101648894963752')) {
+            member.roles.remove('915101648894963752')
+            interaction.reply({ content: `You've been removed from the \`Announcement Pings\` role!`, ephemeral: true })
+        }
+
+    } if (option === 'tourney') {
+        if (!member.roles.cache.has('915101649620578314')) {
+            member.roles.add('915101649620578314')
+            interaction.reply({ content: `You've been given the \`Tournament Pings\` role!`, ephemeral: true })
+        }
+        if (member.roles.cache.has('915101649620578314')) {
+            member.roles.remove('915101649620578314')
+            interaction.reply({ content: `You've been removed from the \`Tournament Pings\` role!`, ephemeral: true })
+        }
+
+    } if (option === 'twitch') {
+        if (!member.roles.cache.has('915101650430087198')) {
+            member.roles.add('915101650430087198')
+            interaction.reply({ content: `You've been given the \`Twitch Pings\` role!`, ephemeral: true })
+        }
+        if (member.roles.cache.has('915101650430087198')) {
+            member.roles.remove('915101650430087198')
+            interaction.reply({ content: `You've been removed from the \`Twitch Pings\` role!`, ephemeral: true })
+        }
+
+    } if (option === 'poll') {
+        if (!member.roles.cache.has('915101651948408832')) {
+            member.roles.add('915101651948408832')
+            interaction.reply({ content: `You've been given the \`Poll Pings\` role!`, ephemeral: true })
+        }
+        if (member.roles.cache.has('915101651948408832')) {
+            member.roles.remove('915101651948408832')
+            interaction.reply({ content: `You've been removed from the \`Poll Pings\` role!`, ephemeral: true })
         }
     }
 }
