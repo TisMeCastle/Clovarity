45b77b02d9441a14dd150005ff556f259a77a383
TisMeCastle <tismecastle@gmail.com>
2023-09-26 18:38:35 -0600

    please work

--- commands/graphics/playerBanner.js
+++ commands/graphics/playerBanner.js
@@ -22,19 +22,29 @@ module.exports = {
     async execute(interaction) {
         interaction.deferReply()
 
-        var fs = require('fs')
+		var fs = require('fs')
+		await fs.readFile("./commands/graphics/PlayerBanner2.0.svg", 'utf8', async function (err, data) {
+			if (err) {
+				return console.log(err);
+			}
+			const r = data.replace('theirnamehere', `${interaction.options.getString("playerbanner")}`);
 
-        await fs.readFile("./commands/graphics/PlayerBanner2.0.svg", 'utf8', async function (err, data) {
-            if (err) {
-                return console.log(err);
-            }
-            var r = data.replace('theirnamehere', `${interaction.options.getString("their_name").toUpperCase()}`)
 
-            fs.writeFile('./commands/graphics/PlayerBanner2.0result.svg', r, function (err) {
-                if (err) return console.log(err);
-            });
+			fs.writeFile('./commands/graphics/PlayerBanner2.0result.svg', r, function (err) {
+				if (err) return console.log(err);
+			});
 
-                const inputFilePath = './commands/graphics/PlayerBanner2.0result.svg'           
+
+			const inputFilePath = './commands/graphics/PlayerBanner2.0result.svg'
+
+			if (fs.existsSync('./commands/graphics/PlayerBanner2.0result.svg')) {
+				console.log('not lit')
+			} else {
+				setTimeout(() => {
+					fs.existsSync('./commands/graphics/PlayerBanner2.0result.svg')
+					console.log('lit')
+				}, 500)
+			}     
 
                 const outputFilePath = await convertFile(inputFilePath, {
                     puppeteer: {
