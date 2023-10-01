c7b63b6118e1f7669563edf2fe9da5db9d1b1da2
TisMeCastle <tismecastle@gmail.com>
2023-09-30 20:23:30 -0600

    Banner Revert (host broken)

--- commands/Graphics/playerBanner.js
+++ commands/Graphics/playerBanner.js
@@ -1,81 +1,58 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { error } = require("console");
 const { convertFile } = require('convert-svg-to-png');
-const sharp = require("sharp")
-var fs = require('fs')
+const fs = require('fs').promises;
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName("playerbanner2")
+        .setName("playerbanner")
         .setDescription("Custom Player Banners")
-        .addStringOption((option) =>
-            option
-                .setName("their_name")
-                .setDescription("What's Their Gamer Tag?")
-                .setRequired(true)
-        )
-        .addStringOption((option) =>
-            option
-                .setName("font_size")
-                .setDescription("Stock is 115px")
-                .setRequired(false)
-        ),
+        .addStringOption(option => option.setName("their_name").setDescription("What's Their Gamer Tag?").setRequired(true))
+        .addStringOption(option => option.setName("font_size").setDescription("Font size on the graphic").setRequired(false)),
+    
     async execute(interaction) {
-        await interaction.deferReply()
+        await interaction.deferReply();
 
-        let fontSize = "115"
-        if (interaction.options.getString("font_size")) {
-            fontSize = interaction.options.getString("font_size")
+        const filePath = './commands/graphics/PlayerBanner2.0result.svg';
+        
+        try {
+            await fs.unlink(filePath);
+        } catch (err) {
+            console.error(`Proper Launch!`);
         }
 
+        try {
+            let data = await fs.readFile("./commands/graphics/PlayerBanner2.0.svg", 'utf8');
+            const replacedName = data.replace('theirnamehere', interaction.options.getString("their_name"));
+            const replacedFontSize = replacedName.replace('fontsizehere', `${interaction.options.getString("font_size")}px`) || '225';
 
-        await fs.readFile("./commands/Graphics/PlayerBanner2.0.svg", 'utf8', async function (err, data) {
-            if (err) {
-                return console.log(err);
-            }
-            var r = data.replace('theirnamehere', `${interaction.options.getString("their_name").toUpperCase()}`)
-            var r1 = r.replace('fontsizehere', `${fontSize}px`)
+            
+			await fs.writeFile('./commands/graphics/PlayerBanner2.0result.svg', replacedFontSize, function (err) {
+				if (err) return console.log(err);
+			});
 
-
-            fs.writeFile('./commands/Graphics/result.svg', r1, function (err) {
-                if (err) return console.log(err);
+            const outputFilePath = await convertFile(filePath, {
+                puppeteer: {
+                    args: ['--no-sandbox', '--disable-setuid-sandbox']
+                },
+                width: 1500,
+                height: 500
             });
 
-            if (fs.existsSync('./commands/Graphics/result.svg')) {
-                console.log('not lit')
-            } else {
-                setTimeout(() => {
-                    fs.existsSync('./commands/Graphics/result.svg')
-                    console.log('lit')
-                }, 500)
-            }
-
-
-            const imageData = await sharp("./commands/Graphics/result.svg")
-                .png()
-                .toFile("./commands/Graphics/result.png")
-                .then(function (info) {
-                })
-                .catch(function (err) {
-                    console.log(err)
-                })
-
-            interaction.editReply({
-                files: [{
-                    attachment: `./commands/Graphics/result.png`,
-                }],
+            await interaction.editReply({
+                files: [outputFilePath],
             });
 
-
-            setTimeout(() => {
+            setTimeout(async () => {
                 try {
-                    fs.unlinkSync('./commands/Graphics/result.png');
-                    fs.unlinkSync('./commands/Graphics/result.svg');
-                    console.log('Files deleted!')
+                    await fs.unlink(`./commands/graphics/PlayerBanner2.0result.png`);
+                    await fs.unlink(filePath);
+                    console.log('Files deleted!');
                 } catch (err) {
                     console.error(err);
                 }
-            }, 10000)
-        })
+            }, 10000);
+        } catch (err) {
+            console.error(`An error occurred: ${err.message}`);
+        }
     }
-};
+}
