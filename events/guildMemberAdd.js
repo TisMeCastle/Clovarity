75fe433cfd0cc5e4882ecdf40ec41e761fee7f40
TisMeCastle <tismecastle@gmail.com>
2023-10-05 13:36:44 -0600

    Fix Welcome Messages

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,8 +1,7 @@
-const Discord = require('discord.js');
 const welcome = '1059568891619201044'
 const rules = '1059569160675393658'
 const MCount = "1016393060512448512"
-const { MessageEmbed, MessageActionRow, MessageButton, Modal, TextInputComponent, MessageSelectMenu } = require('discord.js');
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
 
 module.exports = {
 	name: "guildMemberAdd",
@@ -18,12 +17,11 @@ module.exports = {
 
 			const channel = member.guild.channels.cache.get(welcome)
 			try {
-				channel.send({ content: `\`${member.user.username}\` Welcome To Clovarity!`, embeds: [message] })
+				await channel.send({ content: `\`${member.user.username}\` Welcome To Clovarity!`, embeds: [message] })
 			} catch {
 				member.guild.channels.cache.get('1059563002875084900').send(`<@${member.id}> \`${member.id}\` \`${member.user.tag}\` somehow tried to double send their welcome message!`)
 			}
 
-			//Member Count Updates
 			const MemberCountChannel = member.guild.channels.cache.get(MCount);
 			MemberCountChannel.setName(`🚀┃Members: ${member.guild.memberCount}`);
 
@@ -53,7 +51,6 @@ module.exports = {
 			member.send({ content: "**Select Your Roles!!! Once You've <#1059568873931800637>, Visit <#1059569218451931146> For A Wider Selection!!!**\n*Click The Button Again To Remove Roles*", components: [buttonData] })
 				.catch(() => member.guild.channels.cache.get('1059563002875084900').send(`<@${member.id}> \`${member.id}\` \`${member.user.tag}\` has their DM's disabled!`))
 
-			//code
 		} catch (error) {
 			client.channels.cache.find(channel => channel.id === '1059563002875084900').send("<@821248918214017034> error: " + error)
 		}
